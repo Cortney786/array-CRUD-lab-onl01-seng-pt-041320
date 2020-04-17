c42e857d5c5bff426a2db66bb8ee5cf0bd69177b
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-04-17 17:19:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
  add_element_to_end_of_array = ["lil' bub","grumy cat"]
- 
+ add_element_to_end_of_array.push("nala cat")
 end
 
 def add_element_to_start_of_array(array, element)
